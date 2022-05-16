8a8a6300cdc961b8923e45b2dc9760e95d7ba6b5
OMRFRKYL <omeryolu16@gmail.com>
2022-05-16 17:18:57 +0300

    .env add

--- django-assigments/mysite/settings.py
+++ django-assigments/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qbagc(dzb0w%4)7wxs98$en2$0!yoz$4-%dzpxu29@h=)f(=ab'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
